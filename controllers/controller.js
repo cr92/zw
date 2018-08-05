3af3caf026e18663c419b9720c844282e18af13f
Roy, Agnimitra <Agnimitra.Roy@ellucian.com>
2018-08-05 15:02:29 +0530

    crud apis working

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,32 +7,48 @@ module.exports = {
         let employeeData = req.body;
         Employee
             .create(employeeData)
-            .then((employee) => res.send(employee))
+            .then((employee) => res.status(201).send(employee))
             .catch(next);
     },
 
     readEmployeeById: function (req, res, next) {
-        res.status(200).send({
-            a: 2
-        });
+        let employeeId = req.params.id;
+        Employee
+            .findOne({
+                _id: employeeId
+            })
+            .then((employee) => res.status(200).send(employee))
+            .catch(next);
     },
 
     readAllEmployees: function (req, res, next) {
-        console.log(req.query);
-        res.status(200).send({
-            a: 3
-        });
+        Employee
+            .find({})
+            .then((employees) => res.status(200).send(employees))
+            .catch(next);
     },
 
     updateEmployeeById: function (req, res, next) {
-        res.status(200).send({
-            a: 1
-        });
+        let employeeId = req.params.id;
+        let employeeData = req.body;
+        Employee
+            .findByIdAndUpdate({
+                _id: employeeId
+            }, employeeData)
+            .then(() => Employee.findById({
+                _id: employeeId
+            }))
+            .then((employee) => res.status(200).send(employee))
+            .catch(next);
     },
 
     deleteEmployeeById: function (req, res, next) {
-        res.status(200).send({
-            a: 5
-        });
+        let employeeId = req.params.id;
+        Employee
+            .findByIdAndRemove({
+                _id: employeeId
+            })
+            .then((employee) => res.status(204).send(employee))
+            .catch(next);
     }
 }
